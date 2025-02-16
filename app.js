d2eefd27b8a300b534d15c319c3322cb222cf2c3
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2025-02-16 13:44:42 +0530

    deplyment config start

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
- "mongodb+srv://bunty:4ozHNDozysKXwaMc@cluster0.accgv.mongodb.net/shop?w=majority&appName=Cluster0";
-
+// "mongodb+srv://bunty:4ozHNDozysKXwaMc@cluster0.accgv.mongodb.net/shop?w=majority&appName=Cluster0";
+const MONGODB_URI = 
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.accgv.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`;
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
@@ -49,6 +50,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
@@ -112,7 +115,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const stripe = require('stripe')('sk_test_tR3PYbcVNZZ796tH88S4VQ2u');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const PDFDocument = require('pdfkit');
 
